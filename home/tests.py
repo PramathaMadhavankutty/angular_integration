d484853f01bced6fd1745b2a65505bd90ea5de7e
PramathaMadhavankutty <pramatha21992@gmail.com>
2016-07-26 10:43:07 -0400

    include comments about  tests

--- home/tests.py
+++ home/tests.py
@@ -7,15 +7,15 @@ from django.shortcuts import render_to_response
 
 
 class HomePageTest(TestCase):
-    def test_home_page_resolves(self):
+    def test_home_page_resolves(self):  # URL Test
         home_page = resolve('/')
         self.assertEqual(home_page.func, get_index)
 
-    def test_home_page_status_code_is_ok(self):
+    def test_home_page_status_code_is_ok(self):  # Testing views - correct status code
         home_page = self.client.get('/')
         self.assertEquals(home_page.status_code, 200)
 
-    def test_check_content_is_correct(self):
+    def test_check_content_is_correct(self):  # Testing views - correct content & template
         home_page = self.client.get('/')
         self.assertTemplateUsed(home_page, 'index.html')
         home_page_template_output = render_to_response('index.html').content
